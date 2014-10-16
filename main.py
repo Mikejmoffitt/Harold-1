2af4ce2e100fda04aee9c808ab7f504ab6315fef
Michael Moffitt <mikejmoffitt@gmail.com>
2014-10-16 02:42:53 -0400

    Tried to make it a little neater and more modular
    I haven't touched python since freshmen year and I can't even test this, so I hope this works?
    
    Variable names like "dafile" are not only unhelpful but are actively misleading - it was often in fact a directory. Do not feel compelled to re-use variables in multiple contexts, you won't save memory and will just cause confusion. 
    
    Those comments had to go.

--- main.py
+++ main.py
@@ -6,90 +6,127 @@ import random
 import json
 import urllib2
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.flushInput()
-
 timeHour = int(time.strftime('%H'))
 
-# os.system("mplayer -slave -input file=/tmp/mplayer.fifo /home/pi/ding.mp3 &")
-os.system("rm -f /tmp/mplayer.fifo")
-os.system("mkfifo /tmp/mplayer.fifo && mplayer -idle -slave -input file=/tmp/mplayer.fifo 1&>/dev/null &")
-
 playTime = 0
+
+def init(baudrate):
+	os.system("rm -f /tmp/mplayer.fifo")
+	os.system("mkfifo /tmp/mplayer.fifo && mplayer -idle -slave -input file=/tmp/mplayer.fifo 1&>/dev/null &")
+	ser = serial.Serial('/dev/ttyACM0', baudrate)
+# If you enable this "ding" I will remove your gonads
+# os.system("mplayer -slave -input file=/tmp/mplayer.fifo /home/pi/ding.mp3 &")
+	ser.flushInput()
+	return ser
 
 playing = False
 start = ""
 varID = ""
 
-# This is a list of sample songs that will randomly play if the user is misidentified or does not exist!
-songs = [ "/users/u22/stuart/harold.mp3", 
-          "/users/u22/henry/harold.mp3",
-          "/users/u22/mbillow/harold.mp3"
-          "/users/u22/henry/harold/selfie.mp3",
-          "/users/u22/henry/harold/waka.mp3",
-          "/users/u22/henry/harold/topworld.mp3",
-          "/users/u22/henry/harold/heybrother.mp3",
-          "/users/u22/henry/harold/boomclap.mp3",
-          "/users/u22/henry/harold/starships.mp3",
-          "/users/u22/henry/harold/domino.mp3",
-          "/users/u22/henry/harold/cruise.mp3"]
+ser = init(9600)
 
-while 1:
-    try:
-        if not playing:
-            varID = ser.readline()
-            print(varID)
-            if "ready" in varID:
-                varID = ""
-            os.system("echo \"loadfile /home/pi/ding.mp3\" >/tmp/mplayer.fifo")  # Mplayer will play any files sent to the FIFO file.
-    except:
-        pass
-    m = alsaaudio.Mixer(control='PCM')
-    if (23 <= timeHour <= 24) or (0 <= timeHour <= 7):  # Lower the volume during quite hours... Don't piss off the RA!
-        m.setvolume(85)
-    else:
-        m.setvolume(100)
-    if not playing and varID != "":
-        try:
-            usernameData = json.load(urllib2.urlopen('http://www.csh.rit.edu:56124/?ibutton=' + varID))  # Use Nick Depinet's LDAP service!
-        except urllib2.HTTPError, error:
-            # Need to check its an 404, 503, 500, 403 etc.
-            contents = error.read()
-            print(contents)
-            username = ""
-            dafile = songs[random.randint(0, len(songs)-1)]
-        else:
-            username = usernameData['username'][0]
-            dafile = usernameData['homeDir'][0]
-            
-        print("New User: '" + username + "'")  # Print the user's name (Super handy for debugging...)
+quietVolume = 60
 
-        if (username != "") and (os.path.isdir(dafile + "/harold")):  #
+ldapStr = "http://www.csh.rit.edu:56124/?ibutton="
 
-            playlistFiles = [f for f in os.listdir(dafile + "/harold") if os.path.isfile(dafile + "/harold/" + f) and f.endswith(".mp3") ]  # Makes list of files excluding directories.
-            shuffleSong = playlistFiles[random.randint(0, len(playlistFiles)-1)]  # Pick a random song from the list!
-            os.system("echo \"loadfile '" + dafile + "/harold/" + shuffleSong.replace("'", "\\'") + "'\" >/tmp/mplayer.fifo")  # Play dat funky music white boy!
-            #os.system("bash scrapemp3.sh '" + dafile + "/harold'")
+# Pool of random songs for users without harold tracks in place
+songs = [ "/users/u22/stuart/harold.mp3",
+	"/users/u22/henry/harold.mp3",
+	"/users/u22/mbillow/harold.mp3"
+	"/users/u22/henry/harold/selfie.mp3",
+	"/users/u22/henry/harold/waka.mp3",
+	"/users/u22/henry/harold/topworld.mp3",
+	"/users/u22/henry/harold/heybrother.mp3",
+	"/users/u22/henry/harold/boomclap.mp3",
+	"/users/u22/henry/harold/starships.mp3",
+	"/users/u22/henry/harold/domino.mp3",
+	"/users/u22/henry/harold/cruise.mp3"]
 
-        elif (username != "") and (os.access(dafile + "/harold.mp3", os.R_OK)) and not (os.path.isdir(dafile + "/harold")):  # User only has one song, well PLAY IT!
-            print("Now playing '" + username + "'...\n")
-            os.system("echo \"loadfile " + dafile + "/harold.mp3\" >/tmp/mplayer.fifo")
+# Stuff your music into the mplayer FIFO
+def playFile(fpath):
+	os.system("echo \"loadFile '" + fpath + "'\" > /tmp/mplayer.fifo")
 
-        else:
-            print("Error - not found. Now playing '" + dafile + "'...\n")  # User doesn't have ANYTHING?! (or doesn't exist...) Welp, play something from the list.
-            dafile = songs[random.randint(0, len(songs)-1)]
-            os.system("echo \"loadfile " + dafile + "\" >/tmp/mplayer.fifo")
+# Attenuate audio based on the time.
+def setVolume(time):
+	m = alsaaudio.Mixer(control='PCM')
+	if (23 <= time <= 24) or (0 <= time <= 7):
+		m.setvolume(quietvolume)
+	else:
+		m.setvolume(100)
 
-        time.sleep(3)
-        start = time.strftime("%s")
-        playing = True
-    elif playing and int(time.strftime("%s")) - int(start) >= 25:
-        vol = int(m.getvolume()[0])  # Fade out the music at the end.
-        while vol > 60:
-            m.setvolume(vol)
-            time.sleep(0.1)
-            vol -= 1 + 1 / 3 * (100 - vol)
-        os.system("echo \"stop\" >/tmp/mplayer.fifo")
-        playing = False
-        ser.flushInput()
-        print "Stopped\n"
+def fadeOut():
+	vol = int(m.getvolume()[0])
+	# Uh, fade it out to 60 with some goofy math and then cut it off, I guess?
+	while vol > 60:
+		m.setvolume(vol)
+		time.sleep(0.1)
+		vol -= 1 + 1 / 3 * (100 - vol)
+
+# If username retrieval failed, it will play a random song.
+def tryHomeDirHarold(username, directory):
+	if (username != "") and (os.access(directory + "\harold.mp3", os.R_OK)):
+		playFile(directory + "\harold.mp3");
+	else:
+		playFile(songs[random.randint(0, len(songs)-1)]);
+
+def haroldLoop():
+	while 1:
+		try:
+			if not playing:
+				varID = ser.readline()
+				print(varID)
+				if "ready" in varID:
+					varID = ""
+				# It shouldn't ding.
+				# os.system("echo \"loadfile /home/pi/ding.mp3\" >/tmp/mplayer.fifo")
+		except:
+			pass
+
+		setVolume(timeHour)
+		if not playing and varID != "":
+			try:
+				# Get username from ibutton using this LDAP service
+				usernameData = json.load(urllib2.urlopen(ldapStr + varID))
+			except urllib2.HTTPError, error:
+				# Check for returned error codes and do nothing with them
+				contents = error.read()
+				print(contents)
+				username = ""
+
+				# Not sure why this is set here; it would have been clearer if it wasn't
+				# a re-used variable name called "dafile".
+				homedir = songs[random.randint(0, len(songs)-1)]
+			else:
+				username = usernameData['username'][0]
+				homedir = usernameData['homeDir'][0]
+
+			print("New User: '" + username + "'")
+
+			# Username retrieved, harold directory exists
+			if (username != "") and (os.path.isdir(homedir + "/harold")):
+				# Get a list of the files in the harold directory, choose one random file and play it
+				playlistFiles = [f for f in os.listdir(homedir + "/harold") if os.path.isfile(homedir + "/harold/" + f) and f.endswith(".mp3") ]
+				shuffleSong = playlistFiles[random.randint(0, len(playlistFiles)-1)]
+				if len(playlistFiles) == 0:
+					tryHomeDirHarold(username, homedir)
+				else:
+					playFile(homedir + "/harold/" + shuffleSong.replace("'", "\\'"))
+			else:
+				tryHomeDirHarold(username, homedir)
+			# Wait for an arbitrary amount of time
+			time.sleep(3)
+			start = time.strftime("%s")
+			playing = True
+
+		elif playing and int(time.strftime("%s")) - int(start) >= 25:
+			fadeOut()
+			os.system("echo \"stop\" >/tmp/mplayer.fifo")
+			playing = False
+			ser.flushInput()
+			print "Stopped\n"
+
+def main():
+	haroldLoop()
+
+if __name == "__main__":
+	main()
